e4b7184c8b64cf5df4602e516b41d6cac147feb5
math10 <sanim_10@yahoo.com>
2017-08-13 21:19:23 +0600

    Complete SVM

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,18 +12,27 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.svm import SVC
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
-
-
+features_train = features_train[:len(features_train)/100] 
+labels_train = labels_train[:len(labels_train)/100]
+#features_test = features_test[:len(features_test)/100] 
+#labels_test = labels_train[:len(labels_test)/100] 
 
 
 #########################################################
 ### your code goes here ###
+clf = SVC(kernel='rbf',C=10000)
+t0 = time()
+clf.fit(features_train,labels_train)
+print "training time:", round(time()-t0, 3), "s"
+t0 = time()
+print(clf.score(features_test,labels_test))
+print "training time:", round(time()-t0, 3), "s"
 
 #########################################################
 
