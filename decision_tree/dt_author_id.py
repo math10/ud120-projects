3ababc2e446a13b2511942c8d4ef103009f0b99b
math10 <sanim_10@yahoo.com>
2017-08-17 22:01:30 +0600

    Complete Decision Tree

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -12,7 +12,7 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.tree import DecisionTreeClassifier
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -20,11 +20,13 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
+print(len(features_train[0]))
 
 #########################################################
 ### your code goes here ###
-
+clf = DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train, labels_train)
+print(clf.score(features_test, labels_test))
 
 #########################################################
 
