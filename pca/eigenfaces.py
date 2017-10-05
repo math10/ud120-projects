7131aa9464c56e9e2dbc1bda18e6bfdf12c46a9b
math10 <sanim_10@yahoo.com>
2017-10-06 00:38:12 +0600

    Completed PCA

--- pca/eigenfaces.py
+++ pca/eigenfaces.py
@@ -66,7 +66,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random
 ###############################################################################
 # Compute a PCA (eigenfaces) on the face dataset (treated as unlabeled
 # dataset): unsupervised feature extraction / dimensionality reduction
-n_components = 150
+n_components = 15
 
 print "Extracting the top %d eigenfaces from %d faces" % (n_components, X_train.shape[0])
 t0 = time()
@@ -97,7 +97,7 @@ clf = clf.fit(X_train_pca, y_train)
 print "done in %0.3fs" % (time() - t0)
 print "Best estimator found by grid search:"
 print clf.best_estimator_
-
+print pca.explained_variance_ratio_ 
 
 ###############################################################################
 # Quantitative evaluation of the model quality on the test set
