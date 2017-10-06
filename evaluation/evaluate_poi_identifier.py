2dfe4c434c6da005999eee72cdb6c3e54be4be15
math10 <sanim_10@yahoo.com>
2017-10-07 01:32:37 +0600

    Completed Validation

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -15,6 +15,8 @@ import pickle
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.cross_validation import train_test_split
 
 data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
 
@@ -29,3 +31,13 @@ labels, features = targetFeatureSplit(data)
 ### your code goes here 
 
 
+clf = DecisionTreeClassifier(random_state=0)
+a_train, a_test, b_train, b_test = train_test_split(features, labels, test_size=0.3, random_state=42)
+clf.fit(a_train, b_train)
+result = clf.predict(a_test)
+l = len(result)
+cnt = 0
+for i in range(l):
+  if result[i] == 1 and b_test[i] == 0:
+    cnt += 1
+print cnt
